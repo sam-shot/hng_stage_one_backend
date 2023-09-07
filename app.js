2be0d80576b6275d46865b482a8fd023c4832741
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-07 15:11:40 +0000

    feat : app.js added

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-aa
+
+const express = require('express');
+const app = express();
+
+app.get('/hng/stageone', (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  const currentTime = new Date().toISOString();
+  
+  const response = {
+    slack_name: slackName,
+    current_day: new Date().toLocaleDateString('en-US', { weekday: 'long' }),
+    utc_time: currentTime,
+    track: track,
+    github_file_url: 'https://github.com/sam-shot/hng_stage_one_backend/blob/main/app.js',
+    github_repo_url: 'https://github.com/sam-shot/hng_stage_one_backend/',
+    status_code: 200,
+  };
+
+  res.send(response);
+});
+app.get('/', (req, res) => {
+  
+
+  res.send({
+    status: "E dey Shake for here"
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
