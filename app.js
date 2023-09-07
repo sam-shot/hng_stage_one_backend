1024dac7511af3887d9b73874349154b57be39b8
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-07 11:51:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
