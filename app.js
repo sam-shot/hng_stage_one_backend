6d8884a6052618ce9f5bd8c367277e4b59b7da4e
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-07 11:11:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/hng/stageone', (req, res) => {
+app.get('/api', (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
 
